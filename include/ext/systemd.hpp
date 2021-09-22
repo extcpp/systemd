a3e51fce7ea38babb515ae53968f0524cc7fc764
Jan Christoph Uhde <Jan@UhdeJC.com>
2021-09-22 22:08:35 +0200

    inital commit

---
+++ include/ext/systemd.hpp
@@ -0,0 +1,16 @@
+// Copyright - 2021 - Jan Christoph Uhde <Jan@UhdeJC.com>
+// Please see LICENSE.md for license or visit https://github.com/extcpp/basics
+#ifndef EXT_SYSTEMD_HEADER
+#define EXT_SYSTEMD_HEADER
+#include <string>
+
+namespace ext::systemd {
+bool active(); // is the feature compiled and active
+bool notify_ready();
+bool notify_alive();
+bool notify_stopping();
+bool notify_error(int errno);
+bool notify_error(std::string const& reason, int errno);
+}
+
+#endif
---
+++ src/systemd.cpp
@@ -0,0 +1,72 @@
+#include <ext/systemd.hpp>
+
+namespace ext::systemd {
+#if __has_include(<systemd/sd-daemon.h>)
+
+extern "C" {
+	#include <systemd/sd-daemon.h>
+    #include <string.h>
+}
+
+bool active() {
+    return true;
+}
+
+bool notify_ready(){
+    return sd_notify(0 /*unset_environment*/, "READY=1") > 0;
+}
+
+bool notify_alive(){
+    return sd_notify(0 /*unset_environment*/, "WATCHDOG=1") > 0;
+}
+
+bool notify_stopping(){
+    return sd_notify(0 /*unset_environment*/, "STOPPING=1") > 0;
+}
+
+bool notify_error(int errno){
+    return sd_notifyf(0 /*unset_environment*/
+                     ,"STATUS=Failed: %s\n"
+                      "ERRNO=%i"
+                     , strerror(errno), errno
+                     ) > 0;
+}
+
+bool notify_error(std::string const& reason, int errno){
+    // use std::format in the future
+    using namespace std::literals::string_literals;
+    std::string block = ( "STATUS=Failed: "s + reason );
+    block += ( "\nERRNO="s + std::to_string(errno) );
+
+    return sd_notify(0 /*unset_environment*/, block.data()) > 0;
+}
+
+#else // not __has_include(<systemd/sd-daemon.h>)
+
+bool active() {
+    return false;
+}
+
+bool notify_ready(){
+    return true;
+}
+
+bool notify_alive(){
+    return true;
+}
+
+bool notify_stopping(){
+    return true;
+}
+
+bool notify_error(int errno){
+    return true;
+}
+
+bool notify_error(std::string const& reason, int errno){
+    return true;
+}
+
+#endif // __has_include(<systemd/sd-daemon.h>)
+
+} // namespace ext::systemd
