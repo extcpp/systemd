1172acc0d87e0bcbde3c0ebc202351dcd52d6673
Jan Christoph Uhde <Jan@UhdeJC.com>
2021-09-22 23:43:53 +0200

    add example

---
+++ examples/daemon.cpp
@@ -0,0 +1,40 @@
+#include <ext/systemd.hpp>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+using namespace std::literals::string_literals;
+using namespace std::literals::chrono_literals;
+
+
+// Simulate a daemon that sends notifications to systemd
+int main() {
+    std::cout << "systemd code is active: " << ext::systemd::active() << std::endl;
+
+    // Simulate daemon startup
+    std::cout << "starting sample daemon:" << std::endl;
+    std::this_thread::sleep_for(3s);
+    std::cout << "starting daemon complete" << std::endl;
+
+    // Now we are ready and tell systemd that it can expect notifications
+    ext::systemd::notify_ready();
+    std::cout << "systemd notified - ready " << std::endl;
+
+    // Send alive notifications in an interval
+    for(int i = 0; i < 10; i++) {
+        std::this_thread::sleep_for(10s);
+        ext::systemd::notify_alive();
+        std::cout << "systemd notified - alive " << std::endl;
+    }
+
+    // Here we simulate that the daemon is not sending further
+    // notifications. Depending on the controlling service
+    // it will be restarted.
+    std::cout << "now forgetting to notify " << std::endl;
+    for (;;) {
+        std::this_thread::sleep_for(10s);
+        std::cout << "process stuck" << std::endl;
+    }
+
+    return 0;
+}
